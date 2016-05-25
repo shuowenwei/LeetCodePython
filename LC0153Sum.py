b3161756b20405989f9620b7ed240fe709844ce2
Shuowen Wei <weisw9@gmail.com>
2016-05-25 01:28:31 -0400

    add solutions to 3Sum

---
+++ LC0153Sum.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/3sum/
+
+"""
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        length = len(nums)
+        if nums is None or length <3:
+            return res   
+            
+        nums.sort()
+        # here should be len-2 
+        for i in range(length-2): 
+            # skip duplicate tuples 
+            if( i!=0 and nums[i-1] == nums[i]):
+                continue
+            
+            left = i + 1 
+            right = length - 1
+            while( left < right):
+                threeSum = nums[i]+nums[left]+nums[right]
+                if threeSum == 0:
+                    res.append( [nums[i], nums[left], nums[right]] )
+                    left = left + 1
+                    right = right - 1
+                    # skip duplicate tuples 
+                    while( left < right and nums[left] == nums[left-1] ):
+                        left = left + 1 
+                    # skip duplicate tuples 
+                    while( left < right and nums[right] == nums[right+1] ):
+                        right = right - 1
+                if threeSum > 0:
+                    right = right - 1
+                if threeSum < 0:
+                    left = left + 1 
+                    
+        return res  
