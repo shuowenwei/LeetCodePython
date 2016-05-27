46ad0d758c30d581babc56f0fd65d65a732c4650
Shuowen Wei <weisw9@gmail.com>
2016-05-26 23:58:26 -0400

    add solutoin 2 to LC066

---
+++ LC066PlusOne _S2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/plus-one/
+
+"""
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        
+        stringNumber = ""
+        for i in digits:
+            stringNumber = stringNumber+str(i)
+        results = str(int(stringNumber)+1) 
+        return [int(e) for e in results]
+        
+        #return [int(i) for i in str(int(''.join([str(i) for i in digits]))+1)]
