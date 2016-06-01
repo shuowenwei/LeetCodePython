2b0bfd68a7c0cf8bf5ced6de2e6095ad4a64cf7b
Shuowen Wei <weisw9@gmail.com>
2016-05-31 23:56:24 -0400

    add solutions to LC141

---
+++ LC141LinkedListCycle.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/linked-list-cycle/
+
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        fast, slow = head, head
+        while fast!=None and fast.next!=None:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
+        
+            
+        
