9d73d6e506ca2e763b635dc04d37bbe00e88a4ad
Wei, Shuowen <Shuowen.Wei@finra.org>
2018-11-29 16:36:47 -0500

    add one solution

---
+++ Medium/LC494TargetSum.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/
+
+solution reference: https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/discuss/139609/python-iterative-and-recursive-solution
+
+
+"""
+class Solution(object):
+    def findTargetSumWays(self, nums, S):
+        """
+        :type nums: List[int]
+        :type S: int
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        dic = {nums[0]: 1, -nums[0]: 1} if nums[0] != 0 else {0: 2}
+        for i in range(1, len(nums)):
+            tdic = {}
+            for d in dic:
+                tdic[d + nums[i]] = tdic.get(d + nums[i], 0) + dic.get(d, 0)
+                tdic[d - nums[i]] = tdic.get(d - nums[i], 0) + dic.get(d, 0)
+            dic = tdic
+        return dic.get(S, 0)
+    
+        """    
+        def findTarget(i, s):
+            if (i, s) not in cache:
+                r = 0
+                if i == len(nums):
+                    if s == 0:
+                        r = 1
+                else:
+                    r = findTarget(i+1, s-nums[i]) + findTarget(i+1, s+nums[i])
+                cache[(i, s)] = r
+            return cache[(i, s)]
+        
+        cache = {}
+        return findTarget(0, S)
+        """
