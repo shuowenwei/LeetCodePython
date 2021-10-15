b6880c17f5898135799e6c07e7e14ad163c43fae
Shuowen Wei <Shuowen.Wei@finra.org>
2021-10-15 10:36:14 -0400

    add framework_tree_BFS.py

---
+++ framework_tree_BFS.py
@@ -0,0 +1,33 @@
+# https://labuladong.gitee.io/algo/1/5/
+
+
+# /* 基本的二叉树节点 */
+class TreeNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+def traverse(root: TreeNode):
+    traverse(root.left)
+    traverse(root.right)
+
+
+# /* 基本的 N 叉树节点 */
+class N_TreeNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.left = []
+def traverse(root: N_TreeNode):
+    for child in root.N_TreeNode: 
+        traverse(root.right)
+
+
+
+def traverse(root: TreeNode):
+    # 前序遍历代码位置
+    traverse(root.left)
+    # 中序遍历代码位置
+    traverse(root.right)
+    # 后序遍历代码位置
+
+
