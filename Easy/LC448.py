3edb598e73a9f77b3d4fff766df74971a9ee61aa
Shuowen Wei <Shuowen.Wei@finra.org>
2021-11-03 17:46:03 -0400

    448

---
+++ Easy/LC448.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+solution: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/discuss/313703/Python-3
+
+"""
+
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for n in nums:
+            a = abs(n) - 1 
+            # print(a)
+            if nums[a] > 0:
+                nums[a] = nums[a] * -1 
+                # print(nums)
+            
+        return [i+1 for i in range(len(nums)) if nums[i] > 0]
