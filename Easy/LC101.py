2ccfdeca6ec7de18d95222a9a82827db20dcf806
Shuowen Wei <Shuowen.Wei@finra.org>
2021-11-03 18:58:35 -0400

    101

--- Easy/LC101.py
+++ Easy/LC101.py
@@ -33,3 +33,23 @@ class Solution(object):
             return self.isSameTree(left.left, right.right) and self.isSameTree(left.right, right.left)
         else:
             return False 
+        
+        """
+        # solution 2: 
+        if root is None:
+            return True
+
+        def helper(node1, node2):
+            if node1 is None and node2 is None:
+                return True
+            elif node1 is not None and node2 is None:
+                return False 
+            elif node1 is None and node2 is not None:
+                return False
+            elif node1.val != node2.val:
+                return False
+            else:
+                return helper(node1.left, node2.right) and helper(node1.right, node2.left)
+            
+        return helper(root.left, root.right)
+        """
