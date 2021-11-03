a10967f424c381d364d6750e397f31d24ae53089
Shuowen Wei <Shuowen.Wei@finra.org>
2021-11-03 12:50:51 -0400

    LC88 and rename/remove 0

--- Easy/LC88.py
+++ Easy/LC88.py
@@ -23,3 +23,23 @@ class Solution(object):
                 nums1[m + n - 1] = nums2[n - 1]
                 n -= 1
         nums1[:n] = nums2[:n]
+        """
+        if n == 0:
+            return nums1 
+        nums1_sz = len(nums1)
+        for i in range(nums1_sz-1, -1, -1):
+            if m > 0 and n > 0:
+                if nums1[m-1] < nums2[n-1]:
+                    nums1[i] = nums2[n-1]
+                    n -= 1
+                else: 
+                    nums1[i] = nums1[m-1]
+                    m -= 1
+                continue
+            if m > 0:
+                nums1[i] = nums1[m-1]
+                m -= 1
+            if n > 0:
+                nums1[i] = nums2[n-1]
+                n -= 1
+        """
