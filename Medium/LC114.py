a78d167b6838f6eed98dbd3d21ace9644cd5637f
Shuowen Wei <Shuowen.Wei@finra.org>
2021-11-04 17:50:52 -0400

    114 - hard

--- Medium/LC114.py
+++ Medium/LC114.py
@@ -2,9 +2,10 @@
 """
 @author: Wei, Shuowen
 
-https://leetcode.com/problems/invert-binary-tree/
+https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
 
 """
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -17,9 +18,9 @@ class Solution(object):
         :type root: TreeNode
         :rtype: None Do not return anything, modify root in-place instead.
         """
-        """ # solution: postorder recursion 
         if root is None:
             return
+        
         self.flatten(root.left)
         self.flatten(root.right)
         
@@ -33,25 +34,4 @@ class Solution(object):
         while p.right is not None:
             p = p.right    
         p.right = rightTreeStartNode
-        """
-        # solution, get preOrder list ? 
-        if root is None:
-            return
-        
-        preOrder = []
-        def traverse(root):
-            if root is None:
-                return 
-            preOrder.append(root.val)
-            traverse(root.left)
-            traverse(root.right)
-        
-        traverse(root)
-        if len(preOrder) <= 1:
-            return 
-        root.left = None 
-        p = root 
-        for val in preOrder[1:]: 
-            p.right = TreeNode(val)
-            p = p.right 
         
