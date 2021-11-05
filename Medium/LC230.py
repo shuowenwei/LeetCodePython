d9b7133af14089436ee74b1930b3af8988e7619c
Shuowen Wei <Shuowen.Wei@finra.org>
2021-11-05 15:38:04 -0400

    230

---
+++ Medium/LC230.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """    
+        res = []
+        def traversal(root):
+            if root is None:
+                return
+            traversal(root.left)
+            res.append(root.val)
+            if len(res) == k:
+                return 
+            traversal(root.right)
+            
+        traversal(root)
+        return res[k-1]
+        
