36ae0210c6e70aa546522d95d725516275fbd366
Shuowen Wei <Shuowen.Wei@finra.org>
2021-12-01 23:27:10 -0500

    121

--- Easy/LC121.py
+++ Easy/LC121.py
@@ -4,6 +4,8 @@
 
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
+labuladong: https://labuladong.gitee.io/algo/1/11/
+
 solution link: 
  	https://leetcode.com/discuss/97355/share-my-simple-python-code-using-method-with-explaination
 
@@ -13,6 +15,24 @@ class Solution(object):
         """
         :type prices: List[int]
         :rtype: int
+        """
+        import numpy as np 
+        n = len(prices)
+        # k = 1, dp[i-1][0][0] is always 0
+        dp = np.zeros((n,2)) # 0: no stock, 1: have stock
+        for i in range(n):
+            if i-1 == -1:
+                dp[i][0] = 0
+                # dp[i][0] = max(dp[-1][0], dp[-1][1] + prices[i])
+                #          = max(0, -infinity + prices[i]) = 0 
+                dp[i][1] = -prices[i]
+                # dp[i][1] = max(dp[-1][1], dp[-1][0] - prices[i])
+                #          = max(-infinity, 0 - prices[i]) =  -prices[i]
+                continue 
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+            dp[i][1] = max(dp[i-1][1], 0 - prices[i]) # dp[i-1][0][0] is always 0
+        return int(dp[n-1][0]) # at (n-1)th day, no stock left 
+        
         """
         if len(prices) < 2:
             return 0 
@@ -24,6 +44,7 @@ class Solution(object):
             runningSum = max(p, p + runningSum)
             maxAll = max(runningSum, maxAll)
         return maxAll
+        """
 
         """
         if len(prices) < 2:
