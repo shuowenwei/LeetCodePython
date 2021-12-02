7bfe1cb67ac87c74b9d6195c4be51a1f67caf815
Shuowen Wei <Shuowen.Wei@finra.org>
2021-12-02 00:06:52 -0500

    188

--- Hard/LC188.py
+++ Hard/LC188.py
@@ -2,7 +2,7 @@
 """
 @author: Wei, Shuowen
 
-https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
 
 labuladong: https://labuladong.gitee.io/algo/1/11/
 
@@ -10,14 +10,32 @@ LC121, LC122, LC123, LC188, LC309, LC714
 
 """
 class Solution(object):
-    def maxProfit(self, prices):
+    def maxProfit(self, k, prices):
         """
+        :type k: int
         :type prices: List[int]
         :rtype: int
         """
         import numpy as np 
         n = len(prices)
-        max_k = 2 # hence k=0 and k=1 are the same 
+        if n <= 0:
+            return 0
+        if k > n/2: # same as k = +infinity, hence k and k-1 are the same 
+            dp = np.zeros((n,2)) # 0: no stock, 1: have stock
+            for i in range(n):
+                if i-1 == -1:
+                    dp[i][0] = 0
+                    # dp[i][0] = max(dp[-1][0], dp[-1][1] + prices[i])
+                    #          = max(0, -infinity + prices[i]) = 0 
+                    dp[i][1] = -prices[i]
+                    # dp[i][1] = max(dp[-1][1], dp[-1][0] - prices[i])
+                    #          = max(-infinity, 0 - prices[i]) =  -prices[i]
+                    continue 
+                dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+                dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+            return int(dp[n-1][0]) # at (n-1)th day, no stock left
+
+        max_k = k # hence k=0 and k=1 are the same 
         dp = np.zeros((n, max_k+1, 2)) # 0: no stock, 1: have stock
         for i in range(n):
             for k in range(max_k, 0, -1):
@@ -32,5 +50,3 @@ class Solution(object):
                 dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
                 dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
         return int(dp[n-1][max_k][0]) # at (n-1)th day, no stock left
-
-
