211cc250c5bca8924036d4286e2f2c9b9dc34203
Shuowen Wei <Shuowen.Wei@finra.org>
2021-12-09 23:59:26 -0500

    213

---
+++ Medium/LC213.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/house-robber-ii/
+
+labuladong: https://mp.weixin.qq.com/s/z44hk0MW14_mAQd7988mfw
+
+LC198, LC213, LC337
+
+"""
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 1: # nums = [1], expecting 1
+            return nums[0]
+        dp1 = [0]*(len(nums)+2)
+        for i in range(len(nums)-2, -1, -1):
+            dp1[i] = max(dp1[i+1], nums[i] + dp1[i+2])
+
+        dp2 = [0]*(len(nums)+2)
+        for i in range(len(nums)-1, 0, -1):
+            dp2[i] = max(dp2[i+1], nums[i] + dp2[i+2])
+
+        return max(dp1[0], dp2[1])
+
+
