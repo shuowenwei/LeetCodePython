b953e6263513da6be702339b89deb67c9a346972
Shuowen Wei <Shuowen.Wei@finra.org>
2021-12-15 23:12:28 -0500

    1135

---
+++ Medium/LC1135.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/connecting-cities-with-minimum-cost/
+
+https://labuladong.gitee.io/algo/2/19/39/
+
+LC323, LC130, LC990
+LC547
+LC261, LC1135, LC1584
+
+"""
+class Solution(object):
+    def validTree(self, n, connections):
+        """
+        :type b: int
+        :type connections: List[List[int]]
+        :rtype: int
+        """
+        parent = [i for i in range(n+1)]
+        rank = [1]*(n+1)
+        uf_count = n+1  
+        minSpanningTree = 0 
+        def find(x): 
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        def union(n1, n2):
+            p1 = find(n1)
+            p2 = find(n2)
+            if p1 == p2:
+                return 0
+            if rank[p1] > rank[p2]:
+                parent[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
+            return 1
+        def isConnected(n1, n2):
+            return find(n1) == find(n2)
+        
+        connections = sorted(connections, lambda x: x[2])
+        for u,v,w in connections:
+            if isConnected(u, v):
+                continue
+            union(u, v)
+            minSpanningTree += w 
+            uf_count -= 1 
+        return minSpanningTree if uf_count == 2 else -1 
--- Medium/LC1584.py
+++ Medium/LC1584.py
@@ -5,6 +5,7 @@
 https://leetcode.com/problems/min-cost-to-connect-all-points/
 
 https://labuladong.gitee.io/algo/2/19/39/
+https://mp.weixin.qq.com/s/dJ9gqR3RVoeGnATlpMG39w
 
 LC323, LC130, LC990
 LC547
