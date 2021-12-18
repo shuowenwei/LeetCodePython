7a82e7eff6c206d99958330af460dcca4101ef33
Shuowen Wei <Shuowen.Wei@finra.org>
2021-12-18 14:40:37 -0500

    debug and LRU

--- fmk_LRU.py
+++ fmk_LRU.py
@@ -14,23 +14,23 @@ class Node(object):
         self.val = val
         self.next = next
         self.prev = prev
-        
+
 
 class DoubleList(object): 
     # // 初始化双向链表的数据
-    def __init__(self, capacity, head=Node(0,0), tail=Node(0,0) ):
+    def __init__(self, head=Node(0,0), tail=Node(0,0) ):
         self.head = head
         self.tail = tail
         self.head.next = self.tail 
         self.tail.prev = self.head 
-        self.size = capacity
+        self.size = 0
         
     # // 在链表尾部添加节点 x，时间 O(1)
-    def addLast(self, x: Node): 
+    def addLast(self, x): 
         x.prev = self.tail.prev
         x.next = self.tail
-        tail.prev.next = x 
-        tail.prev = x 
+        self.tail.prev.next = x 
+        self.tail.prev = x 
         self.size += 1
         
     # // 删除链表中的 x 节点（x 一定存在）
@@ -51,3 +51,45 @@ class DoubleList(object):
     # // 返回链表长度，时间 O(1)
     def getSize(self):
         return self.size
+
+class LRU(object): 
+    def __init__(self, capacity):
+        self.map = dict() # k: node --> v: val
+        self.cap = capacity
+        self.cache = DoubleList()
+    
+    def makeRecently(self, key):
+        node = self.map[key]
+        self.cache.remove(node) # // 先从链表中删除这个节点
+        self.cache.addLast(node) # // 重新插到队尾
+        
+    def addRecently(self, key, val):
+        newNode = Node(key, val)
+        self.cache.addLast(newNode) # // 链表尾部就是最近使用的元素
+        self.map[key] = newNode # // 别忘了在 map 中添加 key 的映射
+        
+    def deleteKey(self, key): 
+        node = self.map[key]
+        self.cache.remove(node) # // 从链表中删除
+        self.map.pop(key) # // 从 map 中删除
+
+    def removeLeastRecently(self):
+        deletedNode = self.cache.removeFirst()
+        deletedKey = deletedNode.key
+        self.map.pop(key)
+        
+    def get(self, key: int): 
+        if self.map.get(key) is None:
+            return -1 
+        self.makeRecently(key)
+        return self.map[key].val
+    
+    def put(key: int, val: int):
+        if self.map.get(key): 
+            self.deleteKey(key)
+            self.addRecently(key, val)
+         
+        if self.cap == self.cache.getSize():
+            self.removeLeastRecently()
+            
+        self.addRecently(key, val)
