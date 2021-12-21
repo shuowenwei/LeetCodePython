7c35044354f00dec2c1bee0c7ee0912f67efa0dc
Shuowen Wei <Shuowen.Wei@finra.org>
2021-12-20 23:37:41 -0500

    LC503

---
+++ Medium/LC503.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/next-greater-element-ii/
+
+https://labuladong.gitee.io/algo/2/20/48/
+
+LC496, LC739, LC503
+
+"""
+class Solution(object):
+    def nextGreaterElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # 对于这种需求，常用套路就是将数组长度翻倍：
+        n = len(nums)
+        res = [0]*2*n
+        stack = []
+        for i in range(2*n-1, -1, -1):
+            # while stack and stack[-1] <= nums[i]:
+            #         stack.pop()
+            while stack:
+                if stack[-1] <= nums[i%n]:
+                    stack.pop()
+                else:
+                    break
+            if len(stack) == 0:
+                res[i] = - 1
+            else:
+                res[i] = stack[-1]
+            stack.append(nums[i%n])
+        
+        return res[:len(nums)]
+    
+# LC496, LC739, LC503
