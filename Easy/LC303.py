fabf004f664657451a3bc267ccc74a77c35aa2d3
Shuowen Wei <Shuowen.Wei@finra.org>
2021-12-23 18:59:12 -0500

    LC303

---
+++ Easy/LC303.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/range-sum-query-immutable/
+
+https://labuladong.gitee.io/algo/2/21/53/
+
+"""
+class NumArray(object):
+    
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.preSum = {-1: 0}
+        for i, val in enumerate(nums):
+            self.preSum[i] = self.preSum[i-1] + val
+
+    def sumRange(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        return self.preSum[right] - self.preSum[left-1]
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
