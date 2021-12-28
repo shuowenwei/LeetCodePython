a69986e85f7c93251d657120b67c97ed8014e3fd
Shuowen Wei <Shuowen.Wei@finra.org>
2021-12-27 21:57:30 -0500

    875

---
+++ Medium/LC875.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://labuladong.gitee.io/algo/2/21/59/
+https://mp.weixin.qq.com/s/E2cyJwMVxRosaU2-bZyTjA
+
+LC875
+"""
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """ 
+        def f(piles, x): # 注意f(x)是单调递减的
+            hours = 0
+            for p in piles: 
+                hours += p / x
+                if p%x > 0: 
+                    hours += 1
+            return hours 
+
+        left = 1
+        right = 10**9+1 
+        while left < right: 
+            mid = left + (right-left)/2 
+            if f(piles, mid) == h:
+                right = mid 
+            elif f(piles, mid) < h:
+                right = mid 
+            else:
+                left = mid + 1 
+        return left 
+    
+        
