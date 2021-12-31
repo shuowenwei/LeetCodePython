f00ae84bd9401471366b2961c892cdf970f63c17
Shuowen Wei <Shuowen.Wei@finra.org>
2021-12-31 12:03:05 -0500

    LC26

--- Easy/LC26.py
+++ Easy/LC26.py
@@ -4,6 +4,9 @@
 
 https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
+https://labuladong.gitee.io/algo/2/21/63/
+
+LC26, LC83, LC27, LC283
 """
 class Solution(object):
     def removeDuplicates(self, nums):
@@ -11,12 +14,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        slow = 0 
+        slow = 0
         for fast in range(len(nums)):
-            if fast == 0:
-                nums[slow] = nums[fast]
             if nums[fast] != nums[slow]:
                 slow += 1
                 nums[slow] = nums[fast]
-                
-        return slow + 1
+            # else slow not changing, while fast -> fast+1 
+        return slow + 1 
