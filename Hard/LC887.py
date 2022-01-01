63e26272a4532afeb72714c0717ca3d6bac5dff9
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-01 10:32:50 -0500

    887 solution 2

--- Hard/LC887.py
+++ Hard/LC887.py
@@ -47,3 +47,18 @@ class Solution(object):
             dp_table[(k,n)] = res
             return res
         return dp(k,n)
+
+        # solution 2:
+# 1、无论你在哪层楼扔鸡蛋，鸡蛋只可能摔碎或者没摔碎，碎了的话就测楼下，没碎的话就测楼上。
+# 2、无论你上楼还是下楼，总的楼层数 = 楼上的楼层数 + 楼下的楼层数 + 1（当前这层楼）。
+# 根据这个特点，可以写出下面的状态转移方程：
+# dp[k][m] = dp[k][m - 1] + dp[k - 1][m - 1] + 1
+# dp[k][m - 1] 就是楼上的楼层数，因为鸡蛋个数 k 不变，也就是鸡蛋没碎，扔鸡蛋次数 m 减一；
+# dp[k - 1][m - 1] 就是楼下的楼层数，因为鸡蛋个数 k 减一，也就是鸡蛋碎了，同时扔鸡蛋次数 m 减一。
+        # dp_table = [[0 for i in range(n+1)] for j in range(k+1)]
+        # m = 0
+        # while dp_table[k][m] < n: 
+        #     m += 1
+        #     for i in range(1, k+1): 
+        #         dp_table[i][m] = dp_table[i][m-1] + dp_table[i-1][m-1] + 1 
+        # return m 
