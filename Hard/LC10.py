dc0c995b2270edfc889c1f0c9e71eba8ca6ba8cf
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-04 16:44:12 -0500

    LC10 still don't full understand

--- Hard/LC10.py
+++ Hard/LC10.py
@@ -18,6 +18,7 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
+        # solution 1: 
 #        if not p:
 #            return not s
         # when p is empty, and then s is also empty, they match
@@ -34,37 +35,45 @@ class Solution(object):
         else: 
             return first_match and self.isMatch(s[1:], p[1:])
         
-        # solution 2:
-        """
+        # solution 2: dp
+        """ 
         dp_table = {}
+        # 若dp(s,i,p,j) = true，则表示s[i..]可以匹配p[j..]；
+        # 若dp(s,i,p,j) = false，则表示s[i..]无法匹配p[j..]。
         def dp(s, p, i, j):
             # base case:
             if j == len(p):
                 return i == len(s)
             if i == len(s):
                 if (len(p) - j) % 2 == 1:
-                    return
+                    return False
                 while j+1 < len(p):
                     if p[j+1] != '*':
                         return False 
                     j += 2 
                 return True
             
-            # check s[i] p[j] match
+            # check if s[i] and p[j] match
             if (i,j) in dp_table:
                 return dp_table[(i,j)]
             res = False
-            if s[i] == p[j] or p[j] == '.':
+            if s[i] == p[j] or p[j] == '.': # 通配符匹配 0 次或多次
                 if j < len(p)-1 and p[j+1] == '*':
+                    # 将j加 2，i不变，含义就是直接跳过p[j]和之后的通配符，即通配符匹配 0 次：
+                    # 将i加 1，j不变，含义就是p[j]匹配了s[i]，但p[j]还可以继续匹配，即通配符匹配多次的情况：
                     res = dp(s, p, i, j+2) or dp(s, p, i+1, j)
                 else:
+                    # 由于这个条件分支是无*的常规匹配，那么如果s[i] == p[j]，就是i和j分别加一：
                     res = dp(s, p, i+1, j+1)
-            else:
+            else: #通配符匹配 0 次
                 if j < len(p)-1 and p[j+1] == '*':
+                    # 将j加 2，i不变，含义就是直接跳过p[j]和之后的通配符，即通配符匹配 0 次：
                     res = dp(s, p, i, j+2)
                 else:
+                    # 如果没有*通配符，也无法匹配，那只能说明匹配失败了：
                     res = False
             dp_table[(i,j)] = res
             return res
         return dp(s, p, 0, 0)
+
         """
