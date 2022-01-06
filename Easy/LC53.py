e051c107fb9de1d37e94f587d7ff12a00b0ab0ec
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-06 17:38:34 -0500

    LC53, LC1143, LC583, LC712

---
+++ Easy/LC53.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/maximum-subarray/
+
+https://labuladong.gitee.io/algo/3/24/77/
+
+LC53, LC1143, LC583, LC712
+"""
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # 以 nums[i] 为结尾的「最大子数组和」为 dp[i]。
+        dp_table = [0]*len(nums)
+        
+        dp_table[0] = nums[0]
+        for i in range(1, len(nums)):
+            dp_table[i] = max(nums[i], nums[i] + dp_table[i-1])
+        return max(dp_table)
