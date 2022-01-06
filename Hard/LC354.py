01273c4d8590f144a8e0481839c7dcd8b9b611d7
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-06 17:20:19 -0500

    LC300, LC354

--- Hard/LC354.py
+++ Hard/LC354.py
@@ -16,7 +16,6 @@ class Solution(object):
         n = len(envelopes)
         dp_table = [1]*n
         envelopes = sorted(envelopes, key=lambda x: (x[0], -x[1])) # 对其高度 h 进行降序排序
-        res = 
         for i in range(n): 
             for j in range(i, n): 
                 # Time Limit Exceeded	
@@ -25,8 +24,7 @@ class Solution(object):
                 # 对于宽度 w 相同的数对，要对其高度 h 进行降序排序。因为两个宽度相同的信封不能相互包含的，逆序排序保证在 w 相同的数对中最多只选取一个。
                 if envelopes[j][1] > envelopes[i][1]:
                     dp_table[j] = max(dp_table[j], 1 + dp_table[i])
-                    
-        return max(dp_table)
+        return res
 envelopes = []
 sol = Solution()
 res = sol.maxEnvelopes(envelopes)
--- Medium/LC300.py
+++ Medium/LC300.py
@@ -7,7 +7,7 @@ https://leetcode.com/problems/longest-increasing-subsequence/
 https://labuladong.gitee.io/algo/3/23/67/
 
 LC509, LC322
-LC300, LC354
+LC300, LC354 -> binary search
 """
 class Solution(object):
     def lengthOfLIS(self, nums):
@@ -22,3 +22,28 @@ class Solution(object):
                     dp_table[i] = max(dp_table[i], dp_table[j]+1)
         # print(dp_table)
         return max(dp_table) # not dp_table[len(nums)-1]
+        
+        # solution 2: 其实最长递增子序列和一种叫做 patience game 的纸牌游戏有关，甚至有一种排序方法就叫做 patience sorting（耐心排序
+        """
+        piles = 0 # // 牌堆数初始化为 0
+        top = [0]*len(nums)
+        for i in range(len(nums)):
+            poker = nums[i] # // 要处理的扑克牌
+            # /***** 搜索左侧边界的二分查找 *****/
+            left, right = 0, piles
+            while left < right:
+                mid = (right + left)/2
+                if top[mid] > poker:
+                    right = mid 
+                elif top[mid] < poker:
+                    left = mid + 1 
+                else:
+                    right = mid 
+            # /*********************************/
+            # // 没找到合适的牌堆，新建一堆
+            if left == piles:
+                piles += 1 
+            # // 把这张牌放到牌堆顶
+            top[left] = poker 
+        return piles
+        """
