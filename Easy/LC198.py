4c6a30e528c79f60a3556c859519783b5e5d26f2
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-11 21:47:15 -0500

    LC198

--- Easy/LC198.py
+++ Easy/LC198.py
@@ -15,6 +15,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # my solutiion
+        # if len(nums) < 3:
+        #     return max(nums)
+        # dp = [0]*len(nums) # dp[i] means max profit if rubbing [0...i] houses
+        # dp[0] = nums[0]
+        # dp[1] = max(nums[0], nums[1])
+        # for i in range(2, len(nums)):
+        #     dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        # return dp[len(nums)-1]
+    
+    
         # solution 1: dp with memo table, bottom up
         # // dp[i] = x 表示：
         # // 从第 i 间房子开始抢劫，最多能抢到的钱为 x
