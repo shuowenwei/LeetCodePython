3113047025983ff5c710a929163b6b789f87de8b
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-13 16:18:30 -0500

    LC55, LC45

--- Medium/LC45.py
+++ Medium/LC45.py
@@ -2,10 +2,26 @@
 """
 @author: Wei, Shuowen
 
-
+https://leetcode.com/problems/jump-game-ii/
 
 https://labuladong.gitee.io/algo/3/26/102/
 
 LC55, LC45
 """
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        res = [2**32]*n
+        res[0] = 0
+        for i in range(n):
+            for jump in range(nums[i]+1):
+                # print(i, jump, ":", res)
+                if i+jump < n:
+                    res[i+jump] = min(res[i+jump], res[i] + 1)
+        return res[n-1]
+            
         
