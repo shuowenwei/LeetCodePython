faaa2e925ee821c92d3ce5d786555270b9cf708f
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-12 23:03:00 -0500

    LC435, LC452 - greedy

--- Medium/LC452.py
+++ Medium/LC452.py
@@ -14,4 +14,14 @@ class Solution(object):
         :type points: List[List[int]]
         :rtype: int
         """
+        res = 0 
+        points.sort(key=lambda x: x[1]) # x[1] is the close of the interval
+        x = -2**32+1
+        for i in range(len(points)):
+            if x >= points[i][0]:
+                res += 1
+            else:
+                x = points[i][1]
+        return len(points) - res
+        
         
