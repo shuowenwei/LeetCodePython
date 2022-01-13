da3b6afb860420cd9349970c564acad994c43c76
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-12 22:56:56 -0500

    LC452

--- Medium/LC452.py
+++ Medium/LC452.py
@@ -2,24 +2,16 @@
 """
 @author: Wei, Shuowen
 
-https://leetcode.com/problems/non-overlapping-intervals/
+https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
 
 https://labuladong.gitee.io/algo/3/26/99/
 
 LC435, LC452 - greedy
 """
 class Solution(object):
-    def eraseOverlapIntervals(self, intervals):
+    def findMinArrowShots(self, points):
         """
-        :type intervals: List[List[int]]
+        :type points: List[List[int]]
         :rtype: int
         """
-        res = 0 
-        intervals.sort(key=lambda x: x[1]) # x[1] is the close of the interval
-        x = -2**32+1
-        for i in range(len(intervals)):
-            if x > intervals[i][0]:
-                res += 1
-            else:
-                x = intervals[i][1]
-        return res 
+        
