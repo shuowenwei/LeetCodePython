d2c2c46f5547548b73ee151a0a3fa25f33c79f23
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-13 16:31:45 -0500

    LC55, LC45

--- Medium/LC45.py
+++ Medium/LC45.py
@@ -24,4 +24,21 @@ class Solution(object):
                     res[i+jump] = min(res[i+jump], res[i] + 1)
         return res[n-1]
             
-        
+        # solution 2: dp with memo table
+        """
+        dp_table = {}
+        def dp(nums, p):
+            n = len(nums) 
+            if p >= n-1: 
+                return 0
+            if p in dp_table:
+                return dp_table[p]
+            
+            res = 2**32
+            for jump in range(1, nums[p]+1): # must start with 1 here
+                res = min(res, 1 + dp(nums, p+jump))
+                
+            dp_table[p] = res 
+            return res 
+        return dp(nums, 0)
+        """
