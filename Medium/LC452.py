cf9523c2bfd1163516bc2ca8a07c185c16cc6337
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-12 22:56:37 -0500

    LC435, LC452 - greedy

---
+++ Medium/LC452.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/non-overlapping-intervals/
+
+https://labuladong.gitee.io/algo/3/26/99/
+
+LC435, LC452 - greedy
+"""
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        res = 0 
+        intervals.sort(key=lambda x: x[1]) # x[1] is the close of the interval
+        x = -2**32+1
+        for i in range(len(intervals)):
+            if x > intervals[i][0]:
+                res += 1
+            else:
+                x = intervals[i][1]
+        return res 
