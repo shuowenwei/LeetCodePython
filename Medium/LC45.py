33b2e8abfa4124002372f3066172c05276f90bda
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-12 23:53:22 -0500

    LC55, LC45

--- Medium/LC45.py
+++ Medium/LC45.py
@@ -2,16 +2,10 @@
 """
 @author: Wei, Shuowen
 
-https://leetcode.com/problems/jump-game/
+
 
 https://labuladong.gitee.io/algo/3/26/102/
 
 LC55, LC45
 """
-class Solution(object):
-    def canJump(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
         
--- Medium/LC55.py
+++ Medium/LC55.py
@@ -2,9 +2,23 @@
 """
 @author: Wei, Shuowen
 
+https://leetcode.com/problems/jump-game/
 
 https://labuladong.gitee.io/algo/3/26/102/
 
 LC55, LC45
 """
-
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums)
+        res = [False]*n
+        res[0] = True
+        for i in range(n):
+            if res[i] is True:
+                for j in range(nums[i]):
+                    res[i+j] = True
+        return res[n-1]
