23d3641cd3e8c2e69e8cf5fd4de015743dba4ec0
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-15 16:16:47 -0500

    debug LC37

--- Hard/LC37.py
+++ Hard/LC37.py
@@ -17,20 +17,20 @@ class Solution(object):
         :rtype: None Do not return anything, modify board in-place instead.
         """
         row, col = len(board), len(board[0])
-        digits = '123456789'
         def getCandidates(board,i,j):
             res = []
             box_i = i/3
             box_j = j/3
             box_nums = set()
-            for ii in range(3*box_i, 3+3*box_i):
-                for jj in range(3*box_j, 3+3*box_j):
-                    if board[ii][jj] != '.':
-                        box_nums.add(board[i][j])
-            row_nums = set(board[:][j])
-            col_nums = set(board[i][:])
-            for c in digits:
-                if c not in box_nums.union(row_nums).union(col_nums):
+            for bi in range(3*box_i, 3+3*box_i):
+                for bj in range(3*box_j, 3+3*box_j):
+                    if board[bi][bj] != '.':
+                        box_nums.add(board[bi][bj])
+            row_nums = set(board[i][:])
+            col_nums = set([board[cj][j] for cj in range(9)]) # this is different from set(board[:][j])
+            # print('\t', box_nums, row_nums, col_nums)
+            for c in '123456789':
+                if c not in box_nums and c not in row_nums and c not in col_nums:
                     res.append(c)
             return res
         
@@ -39,19 +39,21 @@ class Solution(object):
                 return True
             
             i, j = fills[start]
-            list_candidates = getCandidates(board, i, j)
+            list_candidates = getCandidates(board[::], i, j)
             for c in list_candidates:
+                # print(i, j, start, 'choose -->', c, 'from', list_candidates)
                 board[i][j] = c
                 if backtrack(board, fills, start+1):
                     return True
                 board[i][j] = '.'
-            return False 
+            return False
         
         fills = []
         for i in range(row):
             for j in range(col):
                 if board[i][j] == '.':
                     fills.append((i,j))
-        backtrack(board, fills, 0)
+        # print(fills, len(fills))
+        res = backtrack(board, fills, 0)
+        # print('res', res)
         
-            
