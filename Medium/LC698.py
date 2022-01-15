983fb95c6d62e15c5f43560efa9cec46825fb1f6
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-15 16:23:22 -0500

    LC698, LC78, LC46, LC77, LC22todo

--- Medium/LC698.py
+++ Medium/LC698.py
@@ -6,7 +6,7 @@ https://leetcode.com/problems/partition-to-k-equal-sum-subsets/
 
 https://labuladong.gitee.io/algo/4/29/109/
 
-LC698, LC78, LC46, LC77
+LC698, LC78, LC46, LC77, LC22
 LC51, LC37
 - backtrack
 """
