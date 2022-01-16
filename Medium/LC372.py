3f027a13103252922c0142195837a3752be41993
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-16 16:37:00 -0500

    LC372

--- Medium/LC372.py
+++ Medium/LC372.py
@@ -15,4 +15,25 @@ class Solution(object):
         :type b: List[int]
         :rtype: int
         """
+        base = 1337 
+        
+        # (a * b) % base = (a % base)(b % base) % base
+        def mypow(a, k): # computer a**k = a * a * a * .... * a (in total k 'a')
+            a = a % base
+            res = 1
+            for i in range(k):
+                res = res * a
+                res = res % base
+            return res
+        
+        # a**[1,5,6,4] = a**4 * (a**[1,5,6])**10, hence recursion
+        
+        if len(b) == 0:
+            return 1 
+        last = b.pop()
+        part1 = mypow(a, last)
+        part2 = mypow(self.superPow(a, b), 10)
+        
+        return (part1 * part2) % base 
+            
         
