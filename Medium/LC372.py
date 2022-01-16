a45e0011f67069c9962274fd16a9dbcc16fbdc51
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-16 16:24:09 -0500

    LC172, LC793, LC204, LC372

---
+++ Medium/LC372.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/count-primes/
+
+https://labuladong.gitee.io/algo/4/30/119/
+
+LC172, LC793, LC204, LC372
+"""
+class Solution(object):
+    def superPow(self, a, b):
+        """
+        :type a: int
+        :type b: List[int]
+        :rtype: int
+        """
+        
