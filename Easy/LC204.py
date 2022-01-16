f37eb826b2743b292dcae272fab2c1ad31126bd2
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-16 16:21:11 -0500

    204, Sieve of Eratosthenes

---
+++ Easy/LC204.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/count-primes/
+
+https://labuladong.gitee.io/algo/4/30/118/
+
+"""
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # naive solution: Time Limit Exceeded --- too slow
+        def isPrime(num):
+            for i in range(2, int(num**0.5+1)):
+                if num % i == 0:
+                    return False
+            return True
+        res = 0 
+        for i in range(2, n):
+            if isPrime(i):
+                res += 1
+        return res
+    
+        # solution 2: better but not fast enough 
+        res = 0 
+        isPrime = [True]*n # n-1 is strintly less than n
+        for i in range(2, int(n**0.5+1)):
+            if isPrime[i]:
+                for j in range(i*2, n, i): #<- trick happens here
+                    isPrime[j] = False
+        for i in range(2, n):    
+            if isPrime[i]:
+                res += 1 
+        return res 
+    
+        # solution 3: Sieve of Eratosthenes : O(N * loglogN)
+        if n <= 2:
+            return 0
+        res = 0 
+        isPrime = [True]*n # n-1 is strintly less than n
+        isPrime[0] = False
+        isPrime[1] = False
+        for i in range(2, int(n**0.5+1)):
+            if isPrime[i]:
+                for j in range(i*2, n, i): #让 j 从 i 的平方开始遍历，而不是从 2 * i 开始：
+                    isPrime[j] = False
+        return sum(isPrime) 
