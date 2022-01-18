7a9f3a4e17ded2cc314ed1a5b0492e0f680c991e
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-17 21:54:17 -0500

    LC319

---
+++ Medium/LC319.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/bulb-switcher/
+
+https://labuladong.gitee.io/algo/4/30/124/
+
+LC292, LC877, LC319
+"""
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return int(n**0.5)
