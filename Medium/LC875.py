8f0f5f9543d78d5179251c44d11271b8429c9ed2
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-19 23:35:20 -0500

    LC875

--- Medium/LC875.py
+++ Medium/LC875.py
@@ -16,24 +16,23 @@ class Solution(object):
         :type h: int
         :rtype: int
         """ 
-        def f(piles, x): # 注意f(x)是单调递减的
+        def getHours(k):# 注意f(x)是单调递减的
             hours = 0
-            for p in piles: 
-                hours += p / x
-                if p%x > 0: 
+            for p in piles:
+                hours += p / k
+                if p%k != 0:
                     hours += 1
             return hours 
-
-        left = 1
-        right = 10**9+1 
-        while left < right: 
-            mid = left + (right-left)/2 
-            if f(piles, mid) == h:
-                right = mid 
-            elif f(piles, mid) < h:
-                right = mid 
-            else:
-                left = mid + 1 
-        return left 
+        
+        left, right = 1, 10**9
+        while left <= right: # left boundary template
+            mid = left + (right-left)/2
+            if getHours(mid) == h:
+                right = mid - 1
+            elif getHours(mid) < h:
+                right = mid - 1
+            elif getHours(mid) > h:
+                left = mid + 1
+        return left
     
         
