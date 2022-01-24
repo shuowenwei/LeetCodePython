ce53407d9937e47c5f7689fc1121e2b4f47290f0
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-23 20:40:32 -0500

    855

--- Medium/LC855.py
+++ Medium/LC855.py
@@ -21,7 +21,7 @@ class ExamRoom(object):
         self.avail_last = {}
         self.put_segment(0, self.N - 1)
         
-    def put_segment(self, first, last):
+    def put_segment(self, first, last): # first and last index of the available segment
         if first == 0 or last == self.N - 1:
             priority = last - first
         else:
@@ -30,7 +30,6 @@ class ExamRoom(object):
         segment = [-priority, first, last, True]
         self.avail_first[first] = segment
         self.avail_last[last] = segment
-        
         heappush(self.heap, segment)
         
     def seat(self):
@@ -67,16 +66,17 @@ class ExamRoom(object):
         """
         first = p
         last = p
-        left = p-1
-        right = p+1
+        left = p-1 # left segment's last index  
+        right = p+1 # right segment's first index 
+        # Deleting items in self.heap will break heap invariant and requires subsequent heapify() call that executes in O(n log n) time. Instead we can just mark segments as invalid by setting is_valid flag
         if left >= 0 and left in self.avail_last:
             segment_left = self.avail_last.pop(left)
             segment_left[3] = False
-            first = segment_left[1]
+            first = segment_left[1] # update first
         if right < self.N and right in self.avail_first:
             segment_right = self.avail_first.pop(right)
             segment_right[3] = False
-            last = segment_right[2]
+            last = segment_right[2] # update last
 
         self.put_segment(first, last)
 
