7cda2638265611f5bae00ab9ee0d31887c563dad
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-25 23:08:58 -0500

    LC472

--- Medium/LC472.py
+++ Medium/LC472.py
@@ -10,27 +10,39 @@ LC139, LC140, LC472
 - backtrack
 """
 class Solution(object):
-    def wordBreak(self, s, wordDict):
+    def findAllConcatenatedWordsInADict(self, words):
         """
-        :type s: str
-        :type wordDict: List[str]
+        :type words: List[str]
         :rtype: List[str]
         """
+        # code form LC139
+        def wordBreak(s, wordDict):
+            dp_table = {}
+            dict_word = set(wordDict)
+            def dp(s, dict_word):
+                if s == '' or s in dict_word:
+                    dp_table[s] = True
+                    return True
+                if s in dp_table:
+                    return dp_table[s]
+                res = False
+                for i in range(len(s)):
+                    if s[:i] in wordDict:
+                        dp_table[s[:i]] = True
+                        if dp(s[i:], wordDict) is True:
+                            res = True 
+                            break
+                dp_table[s] = res
+                return res
+            return dp(s, dict_word)
+        
         res = []
-        dp_table = {}
-        dict_word = set(wordDict)
-        def backtracking(s, dict_word, tmp, index):
-            print(index, tmp)
-            if index == len(s):
-                res.append(' '.join(tmp))
-                return
-            for i in range(index, len(s)+1): # this must be len(s)+1 since s[i:j] not including j 
-                # print('i: ', i)
-                if s[index:i] in wordDict:
-                    tmp.append(s[index:i])
-                    # print(tmp, index, i)
-                    backtracking(s, dict_word, tmp, i)
-                    tmp.pop()
-                    
-        backtracking(s, dict_word, [], 0)
-        return res    
+        if len(words) < 2:
+            return res
+        words.sort(key = lambda x:len(x)) #print(words)
+        shorter_words = set()
+        for w in words:
+            if wordBreak(w, shorter_words):
+                res.append(w)
+            shorter_words.add(w)
+        return res 
