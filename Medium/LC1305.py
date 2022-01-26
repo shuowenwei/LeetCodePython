bceedc37521170e149885c0d0d75c49db814d4b7
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-26 15:32:33 -0500

    LC1305

--- Medium/LC1305.py
+++ Medium/LC1305.py
@@ -44,3 +44,66 @@ class Solution(object):
         if p2 < len(res2):
             res += res2[p2:]
         return res
+
+# solution 2: use iterator, refer to LC173
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator(object):
+
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.stack = []
+        self.pushLeftBranch(root)
+        
+    def pushLeftBranch(self, ln):
+        while ln is not None:
+            self.stack.append(ln)
+            ln = ln.left
+            
+    def peek(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1].val 
+    
+    def next(self):
+        """
+        :rtype: int
+        """
+        outnode = self.stack.pop() 
+        self.pushLeftBranch(outnode.right)
+        return outnode.val 
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return len(self.stack) > 0 
+    
+class Solution(object):
+    def getAllElements(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        iter1 = BSTIterator(root1)
+        iter2 = BSTIterator(root2)
+        # print(iter1, iter2, iter1==iter2)
+        while iter1.hasNext() and iter2.hasNext():
+            if iter1.peek() > iter2.peek():
+                res.append(iter2.next())
+            else:
+                res.append(iter1.next())
+        while iter1.hasNext():
+            res.append(iter1.next())
+        while iter2.hasNext():
+            res.append(iter2.next())
+        return res 
