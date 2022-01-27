678eb38b8dffb0a09fb98d2c7f47bc2781430a75
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-26 19:13:21 -0500

    LC729

--- Medium/LC729.py
+++ Medium/LC729.py
@@ -8,7 +8,7 @@ https://leetcode.com/problems/my-calendar-i/
 
 MJ: https://www.1point3acres.com/bbs/thread-841619-1-1.html
 
-LC729
+LC729, LC731, LC732
 """
 class MyCalendar(object):
     
@@ -57,7 +57,46 @@ class MyCalendar(object):
             # print(self.clnd)
         return flag
 
+# solution 2: Binary search tree:
+"""
+class TreeNode():
+    	def __init__(self, start, end):
+		self.s = start
+		self.e = end
+		self.left = None
+		self.right = None
 
+class MyCalendar(object):
+    def __init__(self):
+        self.root = None
+        
+    def insert(self, s, e, node):
+        if s >= node.e:
+            if node.right:
+                return self.insert(s, e, node.right)
+            else:
+                node.right = TreeNode(s, e)
+                return True
+        elif s <= node.s:
+            if node.left:
+                return self.insert(s, e, node.left)
+            else:
+                node.left = TreeNode(s, e)
+                return True
+        else:
+            return False
+        
+    def book(self, start, end):
+        """
+        :type start: int
+        :type end: int
+        :rtype: bool
+        """
+        if not self.root:
+			self.root = TreeNode(start, end)
+			return True
+        return self.insert(start, end, self.root)
+"""
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
 # param_1 = obj.book(start,end)
