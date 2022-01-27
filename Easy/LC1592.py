fba562e941aa9659e06fa82fa8851c962139a7e5
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-27 12:04:28 -0500

    LC1592, LC68

--- Easy/LC1592.py
+++ Easy/LC1592.py
@@ -13,17 +13,11 @@ class Solution(object):
         :rtype: str
         """
         tokens = text.split()
-        if len(tokens) == 1:        
-            if len(tokens[0]) == len(text):
-                return text
-            else:
-                return tokens[0] + ' '*(len(text) - len(tokens[0]))
+        if len(tokens) == 1:
+            return tokens[0] + ' '*(len(text) - len(tokens[0]))
+        
         numSpaces = len(text) - sum(len(t) for t in tokens)
         gapLength = numSpaces / (len(tokens)-1)
-        res = '#'.join(tokens).replace('#', ' '*gapLength)
-        
-        if len(res) == len(text):
-            return res
-        else:
-            return res + ' '*(len(text) - len(res))
+        res = (' '*gapLength).join(tokens)#.replace('#', )
+        return res + ' '*(len(text) - len(res))
 
