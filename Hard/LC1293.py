88e53bd19e398f620706dc6002123b9811235341
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-31 00:07:35 -0500

    1293

---
+++ Hard/LC1293.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/shortest-path-in-a-grid-with-obstacles-elimination/
+
+refer to https://leetcode.com/problems/shortest-path-in-a-grid-with-obstacles-elimination/discuss/451787/Python-O(m*n*k)-BFS-Solution-with-Explanation
+"""
+class Solution(object):
+    def shortestPath(self, grid, k):
+        """
+        :type grid: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        from collections import deque
+        row, col = len(grid), len(grid[0])
+        if row == 1 and col == 1:
+            return 0 
+        if k > row - 1 + col - 1:
+            return row + col - 2
+        def getNeighbors(i, j):
+            neighbors = []
+            if i > 0:
+                neighbors.append((i-1, j))
+            if j > 0:
+                neighbors.append((i, j-1))
+            if i < row-1:
+                neighbors.append((i+1, j))
+            if j < col-1:
+                neighbors.append((i, j+1))
+            return neighbors
+
+        q = deque()
+        visited = set()
+        dp_tabel = {}
+        # initial setup
+        q.append( (0,0,k) )
+        dp_tabel[(0,0,k)] = 0
+        visited.add((0,0,k))
+        # dp_tabel[(row-1,col-1,k)] = -1 # if not ever updated after emptying queue
+        # target = (row-1, col-1)
+        while q:
+            cur_i, cur_j, cur_k = q.popleft()
+            for ni, nj in getNeighbors(cur_i, cur_j):
+                if grid[ni][nj] == 1:
+                    if cur_k > 0 and (ni,nj,cur_k-1) not in visited:
+                        dp_tabel[(ni,nj,cur_k-1)] = dp_tabel[(cur_i,cur_j,cur_k)] + 1
+                        visited.add((ni,nj,cur_k-1))
+                        q.append((ni,nj,cur_k-1))
+                if grid[ni][nj] == 0:
+                    if ni == row - 1 and nj == col -1:
+                        return dp_tabel[(cur_i,cur_j,cur_k)] + 1
+                    if (ni,nj,cur_k) not in visited:
+                        dp_tabel[(ni,nj,cur_k)] = dp_tabel[(cur_i,cur_j,cur_k)] + 1
+                        visited.add((ni,nj,cur_k))
+                        q.append((ni,nj,cur_k))
+        return -1 
