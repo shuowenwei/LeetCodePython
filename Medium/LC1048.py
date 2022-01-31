df93953dea25c65b064ab176d28040c4a3b191eb
Shuowen Wei <Shuowen.Wei@finra.org>
2022-01-30 20:26:10 -0500

    1048

---
+++ Medium/LC1048.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/longest-string-chain/
+
+https://leetcode.com/problems/longest-string-chain/discuss/1013746/Python3-dp
+
+"""
+class Solution(object):
+    def longestStrChain(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        def isPredecessor(wordA, wordB):
+            for i in range(len(wordB)):
+                if wordB[:i] + wordB[i+1:] == wordA:
+                    return True
+            return False
+        
+        dict_length = {}
+        words.sort(key = lambda x: len(x))
+        for w in words:
+            if len(w) not in dict_length:
+                dict_length[len(w)] = [w]
+            else:
+                dict_length[len(w)].append(w)
+                
+        final_res = 0 
+        dp_table = {}
+        for w in words:
+            dp_table[w] = 1
+            if len(w)-1 in dict_length:
+                for pre_word in dict_length[len(w)-1]:
+                    if isPredecessor(pre_word, w):
+                        dp_table[w] = max(dp_table[w], 1 + dp_table[pre_word])
+            final_res = max(final_res, dp_table[w])
+        # print(dp_table)
+        return final_res
