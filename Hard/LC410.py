344b1ded9e9d832a59a643d97206a0735f912705
Shuowen Wei <Shuowen.Wei@finra.org>
2022-02-01 19:48:54 -0500

    monotonic decreasing function

--- Hard/LC410.py
+++ Hard/LC410.py
@@ -16,8 +16,8 @@ class Solution(object):
         :type m: int
         :rtype: int
         """
-        # /* 辅助函数f(x)，若限制最大子数组和为 max，
-        # 计算 nums 至少可以被分割成几个子数组 */
+        # /* 辅助函数f(x)，若限制最大子数组和为 max，计算 nums 至少可以被分割成几个子数组 */
+        # f(x) is a monotonic decreasing function
         def splitArray(nums, maxSum):
             count = 1 # // 至少可以分割的子数组数量
             sectionSum = 0 # // 记录每个子数组的元素和
@@ -29,16 +29,17 @@ class Solution(object):
                     sectionSum += nums[i]
             return count
         
-        low = max(nums)
-        high = sum(nums)
+        left = max(nums)
+        right = sum(nums)
         # find left boundary
-        while low <= high: 
-            mid = low + (high - low)/2 
-            n = splitArray(nums, mid)
-            if n == m:
-                high = mid - 1
-            elif n < m: 
-                high = mid - 1
-            elif n > m: 
-                low = mid + 1 
-        return low 
+        while left <= right: 
+            mid = left + (right - left)/2 
+            numSubarray = splitArray(nums, mid)
+            # f(x) is a monotonic decreasing function
+            if numSubarray < m:
+                right = mid - 1
+            elif numSubarray > m: 
+                left = mid + 1 
+            elif numSubarray == m:
+                right = mid - 1
+        return left 
