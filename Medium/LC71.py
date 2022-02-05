af0865a5728d94b6b247525b30d7758f89871142
Shuowen Wei <weisw9@gmail.com>
2022-02-05 18:35:13 -0500

    71

---
+++ Medium/LC71.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/simplify-path/
+
+"""
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        # path = path.replace('//', '/')
+        stack = []
+        tmp = ''
+        for i in range(len(path)):
+            char = path[i]
+            # print(char, stack)
+            if char != '/': 
+                tmp += char
+            if char == '/' or i == len(path) - 1:
+                if tmp == '..':
+                    if stack:
+                        stack.pop()
+                elif tmp in ('.', ''):
+                    pass
+                else:
+                    stack.append(tmp)
+                tmp = ''
+        return '/'+'/'.join(stack)
+        
