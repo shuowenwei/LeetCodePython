47025eee6f85f71645e891d4d66fc3400e3ebe2f
Shuowen Wei <weisw9@gmail.com>
2022-02-06 00:10:48 -0500

    162

--- Medium/LC162.py
+++ Medium/LC162.py
@@ -4,5 +4,22 @@
 
 https://leetcode.com/problems/find-peak-element/
 
-"""
+https://leetcode.com/problems/find-peak-element/discuss/50259/My-clean-and-readable-python-solution
 
+"""
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        while left < right - 1:
+            mid = left + (right - left) / 2
+            if nums[mid] > nums[mid + 1] and nums[mid] > nums[mid - 1]:
+                return mid 
+            elif nums[mid] < nums[mid+1]:
+                left = mid + 1 
+            else:
+                right = mid - 1
+        return left if nums[left] >= nums[right] else right
