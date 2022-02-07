991589c2eee822672ea5dd8a1e4f2c5a5547db52
Shuowen Wei <weisw9@gmail.com>
2022-02-07 12:26:03 -0500

    LC876

--- Easy/LC876.py
+++ Easy/LC876.py
@@ -4,12 +4,10 @@
 
 https://leetcode.com/problems/middle-of-the-linked-list/
 
-labuladong: https://labuladong.gitee.io/algo/1/6/
+https://labuladong.gitee.io/algo/2/17/16/
 
 LC21, LC23, LC141, LC142, LC876, LC160, LC19
-
 """
-
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
@@ -25,5 +23,5 @@ class Solution(object):
         while fast is not None and fast.next is not None: 
             fast = fast.next.next
             slow = slow.next 
-        return slow 
+        return slow # 需要注意的是，如果链表长度为偶数，也就是说中点有两个的时候，我们这个解法返回的节点是靠后的那个节点。
         
