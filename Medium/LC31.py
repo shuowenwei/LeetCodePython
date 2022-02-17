95307bee1a2e66738d97c761ff1f9dfc036d7470
Shuowen Wei <weisw9@gmail.com>
2022-02-16 23:47:48 -0500

    31

--- Medium/LC31.py
+++ Medium/LC31.py
@@ -6,6 +6,10 @@ https://leetcode.com/problems/next-permutation/
 
 https://leetcode.com/problems/next-permutation/discuss/1179544/Python-Solution-with-easy-understanding-and-comments.-(Bonus%3A-Related-links)
 
+https://leetcode.com/problems/next-permutation/discuss/13994/Readable-code-without-confusing-ij-and-with-explanation
+
+https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+
 """
 class Solution(object):
     def nextPermutation(self, nums):
@@ -17,19 +21,19 @@ class Solution(object):
         # First we'll find the first non-increasing element starting from the end
         while i > 0 and nums[i-1] >= nums[i]:
             i -= 1
+        print('i ', i)
         if i == 0: # nums are in descending order
             nums.reverse()
             return
-        
         # If it's not zero then we'll find the first number grater then nums[i-1] starting from end
         k = i - 1
         while nums[j] <= nums[k]:
             j -= 1 
+        print('k ', k, 'j ', j)
         nums[j], nums[k] = nums[k], nums[j]
         # sequence strating from i to end
-        l, r = k+1, len(nums) - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1
-            r -= 1
-
+        left, right = i, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
