c55bed6575df4655d5abb88eb24015a9af1a7c0c
Shuowen Wei <weisw9@gmail.com>
2022-02-17 18:31:07 -0500

    1570

--- Medium/LC1570TODO.py
+++ Medium/LC1570TODO.py
@@ -14,6 +14,26 @@ class SparseVector:
         result = 0
         for n1, n2 in zip(self.nums, vec.nums):
             result += n1 * n2
-        
         return result
 
+
+# https://goodtecher.com/leetcode-1570-dot-product-of-two-sparse-vectors/
+# class SparseVector(object):
+#     def __init__(self, nums):
+#         self.dict_index2num = {}
+#         for i, n in enumerate(nums):
+#             if n != 0 : 
+#                 self.dict_index2num[i] = n
+
+#     # Return the dotProduct of two sparse vectors
+#     def dotProduct(self, vec):
+#         res = 0
+#         for index, val in self.dict_index2num.items():
+#             res += self.dict_index2num[index] * vec.dict_index2num.get(index, 0)
+#         return res
+
+# nums1 = [1,0,0,2,3]
+# nums2 = [0,3,0,4,0]
+# v1 = SparseVector(nums1)
+# v2 = SparseVector(nums2)
+# print(v1.dotProduct(v2))
