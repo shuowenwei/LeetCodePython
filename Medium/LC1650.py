70bcb3b8cc4f2f88d8a0d8aa532742f2dd94cf60
Shuowen Wei <weisw9@gmail.com>
2022-02-18 21:52:22 -0500

    1650

--- Medium/LC1650.py
+++ Medium/LC1650.py
@@ -5,14 +5,27 @@
 https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/
 
 LC235, LC236, LC1650
+LC1650, LC160, LC142
 """
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-#         self.parent = None
+class TreeNode(object):
+    def __init__(self, x, parent = None):
+        self.val = x
+        self.parent = parent
+        #     3
+        #    / \
+        #   5   8
+        #  / \
+        # 6   2
+        #    / \
+        #   7   4  
+root = TreeNode(3)
+node5 = TreeNode(5, root)
+node8 = TreeNode(8, root)
+node6 = TreeNode(6, node5)
+node2 = TreeNode(2, node5)
+node7 = TreeNode(7, node2)
+node4 = TreeNode(4, node2)
 
 class Solution(object):
     def lowestCommonAncestor(self, p, q):
@@ -22,6 +35,35 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
+        head_p = p
+        head_q = q 
+        while head_p.parent and head_q.parent:
+            head_p = head_p.parent
+            head_q = head_q.parent
+
+        if head_p.parent is None:
+            head_p.parent = p
+            slow, fast, third = q, q, q
+
+        if head_q.parent is None:
+            head_q.parent = q
+            slow, fast, third = p, p, p
+        
+        while True:
+            fast = fast.parent.parent
+            slow = slow.parent 
+            if fast == slow:
+                break
+        
+        while third != slow:
+            third = third.parent
+            slow = slow.parent
+        return slow.val
+
+sol = Solution()
+print(sol.lowestCommonAncestor(node6, node7))
+
+"""     if TreeNode has left and right child
         root = p
         while root.parent != None:
             root = root.parent
@@ -43,4 +85,4 @@ class Solution(object):
                 return None #leftLCA or rightLCA
             
         return helper(root, p, q)
-    
+"""
