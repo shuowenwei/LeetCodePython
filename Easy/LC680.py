b4991862c1f300e00d14faf55f4ae65b9f3f7497
Shuowen Wei <weisw9@gmail.com>
2022-02-25 23:24:13 -0500

    neat solution

--- Easy/LC680.py
+++ Easy/LC680.py
@@ -12,8 +12,8 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        def isPalindrome(s):
-            left, right = 0, len(s) - 1
+        def isPalindrome(s, left, right):
+            # left, right = 0, len(s) - 1
             while left < right:
                 if s[left] != s[right]:
                     return False
@@ -26,10 +26,10 @@ class Solution(object):
             if s[left] == s[right]:
                 left += 1
                 right -= 1
-            elif s[left] == s[right-1] or s[left+1] == s[right]:
-                s1 = s[left: right]
-                s2 = s[left+1: right+1]
-                return isPalindrome(s1) or isPalindrome(s2)
+            # elif s[left] == s[right-1] or s[left+1] == s[right]:
+            #     s1 = s[left: right]
+            #     s2 = s[left+1: right+1]
+            #     return isPalindrome(s1) or isPalindrome(s2)
             else:
-                return False
+                return isPalindrome(s, left+1, right) or isPalindrome(s, left, right-1)
         return True 
