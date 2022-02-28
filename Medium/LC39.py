068cd680fedef5d7fddde4a5616196e3800d9703
Shuowen Wei <weisw9@gmail.com>
2022-02-27 22:18:03 -0500

    LC39, LC40

--- Medium/LC39.py
+++ Medium/LC39.py
@@ -2,14 +2,12 @@
 """
 @author: Wei, Shuowen
 
-https://leetcode.com/problems/combination-sum-ii/
-
-https://leetcode.com/problems/combination-sum-ii/discuss/17020/Python-easy-to-understand-backtracking-solution
+https://leetcode.com/problems/combination-sum/
 
 LC39, LC40
 """
 class Solution(object):
-    def combinationSum2(self, candidates, target):
+    def combinationSum(self, candidates, target):
         """
         :type candidates: List[int]
         :type target: int
@@ -22,13 +20,11 @@ class Solution(object):
                 res.append(path[::])
                 return
             for i in range(index, len(nums)):
-                if i > index and nums[i] == nums[i-1]:
-                    continue
                 if nums[i] > target:
                     break
                 path.append(nums[i])
-                # here, must be i+1, not index+1, this is not full permutation
-                backtracking(nums, target-nums[i], i+1, path) 
+                # here, must be i, this is replaceable
+                backtracking(nums, target-nums[i], i, path) 
                 path.pop()
         backtracking(candidates, target, 0, [])
         return res
