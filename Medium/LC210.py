9ec8d4570d5d02c84ebf9260148a6e35fa6ef293
Shuowen Wei <weisw9@gmail.com>
2022-03-03 23:33:25 -0500

    LC210

--- Medium/LC210.py
+++ Medium/LC210.py
@@ -46,6 +46,32 @@ class Solution(object):
         # print(postorder[::-1])
         return postorder[::-1]
 
+        # BFS
+        import collections
+        indegree = collections.defaultdict(int)
+        graph = collections.defaultdict(set)
+        for thenCanTake, takeFirst in prerequisites:
+            graph[takeFirst].add(thenCanTake)
+            indegree[thenCanTake] += 1
+        
+        count = 0 
+        res = []
+        starters = [i for i in range(numCourses) if indegree[i] == 0]
+        q = collections.deque(starters)
+        while q:
+            cur_node = q.popleft()
+            res.append(cur_node)
+            count += 1 
+            for nei_node in graph[cur_node]:
+                indegree[nei_node] -= 1 
+                if indegree[nei_node] == 0:
+                    q.append(nei_node)
+        # print(res, count)
+        if count != numCourses:
+            # // 存在环，拓扑排序不存在
+            return []
+        return res
+    
 numCourses = 2
 prerequisites = [[1,0]]
 
