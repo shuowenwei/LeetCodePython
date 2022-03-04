99ad564dcf7100ca9c80f80d92fd571e805f5f7f
Shuowen Wei <weisw9@gmail.com>
2022-03-03 23:35:44 -0500

    278

--- Easy/LC278.py
+++ Easy/LC278.py
@@ -5,7 +5,6 @@
 https://leetcode.com/problems/first-bad-version/
 
 """
-
 # The isBadVersion API is already defined for you.
 # @param version, an integer
 # @return a bool
@@ -17,14 +16,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        begin = 1 
-        end = n 
-        while begin + 1 < end: 
-            pivot = (begin+end)/2 
-            if isBadVersion(pivot):
-                end = pivot
+        left = 1
+        right = n
+        while left <= right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid) is True:
+                right = mid - 1 
             else:
-                begin = pivot
-        if isBadVersion(begin):
-            return begin 
-        return end 
+                left = mid + 1
+        return left 
