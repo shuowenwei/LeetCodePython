d060d60a46a5c6ff41847137f6a6e1e6e7dc5541
Shuowen Wei <weisw9@gmail.com>
2022-03-17 17:33:07 -0400

    LC114, LC116

--- Medium/LC114.py
+++ Medium/LC114.py
@@ -4,12 +4,10 @@
 
 https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
 
-https://labuladong.gitee.io/algo/2/18/21/
+https://labuladong.gitee.io/algo/2/19/23/
 
 LC226, LC114, LC116
-
 """
-
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -24,18 +22,40 @@ class Solution(object):
         """
         if root is None:
             return
-        
+        # // 先递归拉平左右子树
         self.flatten(root.left)
         self.flatten(root.right)
         
+        # /****后序遍历位置****/
+        # // 1、左右子树已经被拉平成一条链表
         leftTreeStartNode = root.left
         rightTreeStartNode= root.right
         
+        # // 2、将左子树作为右子树
         root.left = None
         root.right = leftTreeStartNode
         
+        # // 3、将原先的右子树接到当前右子树的末端
         p = root
         while p.right is not None:
             p = p.right    
         p.right = rightTreeStartNode
         
+        # solution 2:
+        """
+        if root is None:
+            return
+        
+        leftTreeStartNode = self.flatten(root.left)
+        rightTreeStartNode = self.flatten(root.right)
+        
+        root.left = None
+        root.right = leftTreeStartNode
+        
+        cur = root
+        while cur.right is not None:
+            cur = cur.right    
+        cur.right = rightTreeStartNode
+        
+        return root 
+        """
--- Medium/LC116.py
+++ Medium/LC116.py
@@ -4,10 +4,9 @@
 
 https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
 
-https://labuladong.gitee.io/algo/2/18/21/
+https://labuladong.gitee.io/algo/2/19/23/
 
 LC226, LC114, LC116
-
 """
 """
 # Definition for a Node.
@@ -18,15 +17,14 @@ class Node(object):
         self.right = right
         self.next = next
 """
-
 class Solution(object):
     def connect(self, root):
         """
         :type root: Node
         :rtype: Node
         """
-        """
         # solution 1: recursively via a helper function 
+        """
         if root is None:
             return root 
         def connectLeft2Right(left, right):
@@ -39,8 +37,9 @@ class Solution(object):
         connectLeft2Right(root.left, root.right)
         return root
         """
-        """
+        
         # solution 2: recursively without a helper function 
+        """
         if root and root.left and root.right: 
             root.left.next = root.right
             if root.next: # 5 is 2's right child, 6 is 3' left child
@@ -49,17 +48,23 @@ class Solution(object):
             self.connect(root.right)
         return root
         """
+        
         # solution 3: BFS
         if root is None:
-            return root 
-        queue = [root]
-        while len(queue) > 0:
-            queue_size = len(queue)
-            for i in range(queue_size):
-                cur = queue.pop(0)
-                if cur.left is not None and cur.right is not None:
-                    queue.append(cur.left)
-                    queue.append(cur.right)
-                if i < queue_size-1:
-                        cur.next = queue[0]
+            return root
+        q = collections.deque()
+        q.append(root)
+        while q:
+            size = len(q)
+            for i in range(size):
+                cur_node = q.popleft()
+                if i < size - 1:
+                    cur_node.next = q[0]
+                # else:
+                #     cur_node.next = None
+                if cur_node.left:
+                    q.append(cur_node.left)
+                if cur_node.right:
+                    q.append(cur_node.right)
         return root
+    
