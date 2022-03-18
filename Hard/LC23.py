0d8b6d13bb1f6b02281f758f8166b42255e12fa1
Shuowen Wei <weisw9@gmail.com>
2022-03-17 21:00:13 -0400

    23

--- Hard/LC23.py
+++ Hard/LC23.py
@@ -4,13 +4,9 @@
 
 https://leetcode.com/problems/merge-k-sorted-lists/
 
-labuladong: https://labuladong.gitee.io/algo/1/6/
+https://labuladong.gitee.io/algo/2/18/17/
 
 LC21, LC23, LC141, LC142, LC876, LC160, LC19
-
-
-one solution: https://leetcode.com/problems/merge-k-sorted-lists/discuss/908846/Python-3-commented-O(n-log(k))-faster-than-90.64
-
 """
 # Definition for singly-linked list.
 # class ListNode(object):
@@ -23,23 +19,20 @@ class Solution(object):
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        # if not lists:
-        #     return None
         from heapq import heappush, heappop 
+        hp = []
         # the dummy_head.next is to be returned 
         dummy_head = ListNode(-1)
         cur = dummy_head
-        # initialize heap
-        hp = []
-        for index, ln_head in enumerate(lists):
+        for ln_head in lists:
             if ln_head:
-                heappush(hp, (ln_head.val, ln_head, index)) # index is not even needed 
+                heappush(hp, (ln_head.val, ln_head)) 
 
         # find the smallest val among the k listnodes
         while hp:
-            _, node, index = heappop(hp)  # index is not even needed 
+            _, node = heappop(hp)
             cur.next = node
             cur = cur.next
             if node.next:
-                heappush(hp, (node.next.val, node.next, index))  # index is not even needed 
+                heappush(hp, (node.next.val, node.next))
         return dummy_head.next
