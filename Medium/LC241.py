19fa9a27f37cb2af9136a13ed208a4df76732e77
Shuowen Wei <weisw9@gmail.com>
2022-03-17 23:13:42 -0400

    241

--- Medium/LC241.py
+++ Medium/LC241.py
@@ -6,6 +6,8 @@ https://leetcode.com/problems/different-ways-to-add-parentheses/
 
 https://labuladong.gitee.io/algo/4/31/130/
 
+https://leetcode.com/problems/different-ways-to-add-parentheses/discuss/1832797/Easy-to-understand-Python-DP-recursive-solution
+
 LC241, LC282, LC43, LC224, LC227, LC772, LC150
 """
 class Solution(object):
@@ -22,7 +24,7 @@ class Solution(object):
         res = []
         for i in range(n):
             sign = expression[i]
-            if sign in ('-', '+', '*'): # no divide /
+            if sign in ('-', '+', '*'): # there's no divide sign: '/'
                 # /****** 分 ******/
                 # // 以运算符为中心，分割成两个字符串，分别递归计算
                 left = self.diffWaysToCompute(expression[0:i]) # not including i
@@ -37,8 +39,6 @@ class Solution(object):
                             res.append(l + r)
                         elif sign == '*':
                             res.append(l * r)
-                        else:
-                            pass
                 # print('left', left, ' -- ', sign, ' -- right ', right, '-->', res)
         # // base case
         # // 如果 res 为空，说明算式是一个数字，没有运算符
@@ -61,3 +61,34 @@ ob.diffWaysToCompute(expression)
 ('left', [2], ' -- ', u'*', ' -- right ', [-1], '-->', [-2])
 ('left', [6], ' -- ', u'-', ' -- right ', [4], '-->', [-2, 2])
 ('left', [-2, 2], ' -- ', u'*', ' -- right ', [5], '-->', [-34, -10, -14, -10, 10])
+
+        # solution 2: dp
+        """
+        def operator_eval(l, r, sign):
+            if sign == '+':
+                return l + r
+            elif sign == '-':
+                return l - r
+            elif sign == '*':
+                return l * r
+            
+        dp_table = {}
+        def dp(exp):
+            if exp in dp_table:
+                return dp_table[exp]
+            if exp.isdigit():
+                dp_table[exp] = [int(exp)]
+            else:
+                res = []
+                for i, sign in enumerate(exp):
+                    if sign in ('-', '+', '*'):
+                        left = dp(exp[:i])
+                        right = dp(exp[i+1:])
+                        for l in left:
+                            for r in right:
+                                res.append(operator_eval(l, r, sign))
+                dp_table[exp] = res
+            return dp_table[exp] 
+        
+        return dp(expression)
+        """
