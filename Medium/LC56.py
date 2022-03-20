6c03c6b487cdce97f22e1c38050329c1e19031c9
Shuowen Wei <weisw9@gmail.com>
2022-03-20 18:16:29 -0400

    56

--- Medium/LC56.py
+++ Medium/LC56.py
@@ -17,22 +17,18 @@ class Solution(object):
         """
         # refer to LC1288
         res = []
-        n = len(intervals)
-        intervals.sort(key=lambda x: (x[0], -x[1])) # 2nd key doesn't matter 
-        left = intervals[0][0]
-        right = intervals[0][1]
-        for i in range(1, n):
-            # no overlap 
-            if right < intervals[i][0]:
-                res.append( [left, right] )
-                left = intervals[i][0]
-                right = intervals[i][1]
+        intervals.sort(key = lambda x: (x[0], x[1]))
+        start = intervals[0][0]
+        end = intervals[0][0]
+        for intvl in intervals:
             # have some intersection/overlap, extend 'right'
-            elif right >= intervals[i][0] and right < intervals[i][1]:
-                right = intervals[i][1]
-            # fully covered
-            elif right >= intervals[i][1]:
-                # pass
-                continue
-        res.append([left, right])
-        return res
+            if intvl[0] <= end:
+                # start = min(start, intvl[0])
+                end = max(end, intvl[1])
+            else: 
+            # no overlap 
+                res.append([start, end]) 
+                start = intvl[0]
+                end = intvl[1]
+        res.append([start, end]) 
+        return res 
