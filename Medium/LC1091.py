401f789004f1868fcabcc8e9e5cd47083ca22a9f
Shuowen Wei <weisw9@gmail.com>
2022-03-20 16:54:44 -0400

    1091

--- Medium/LC1091.py
+++ Medium/LC1091.py
@@ -11,26 +11,26 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        if len(grid) == 0:
+            return - 1
         row, col = len(grid), len(grid[0])
         if grid[0][0] != 0:
             return -1 
         q = collections.deque()
-        q.append((0 ,0))
+        q.append((0, 0, 1))
         visited = set()
-        d = [(1,0),(-1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
-        step = 0
+        directions = [(1,0), (-1,0), (0,1), (0,-1),
+                      (1,1), (1,-1), (-1,1), (-1,-1)]
         while q:
             size = len(q)
-            step += 1
-            for k in range(size):
-                i, j = q.popleft()
-                if i == row - 1 and j == col - 1:
-                    return step 
-                for di, dj in d:
-                    ni, nj = i + di, j + dj
-                    if 0<=ni<=row-1 and 0<=nj<=col-1 and (ni,nj) not in visited and grid[ni][nj] == 0:
-                        q.append( (ni,nj) )
-                        visited.add( (ni,nj) )
-            # print(q)
+            i, j, step = q.popleft()
+            if (i,j) == (row - 1, col - 1):
+                return step 
+            for di, dj in directions:
+                ni = i + di
+                nj = j + dj
+                if 0<=ni<=row-1 and 0<=nj<=col-1 and (ni,nj) not in visited and grid[ni][nj] == 0:
+                    q.append((ni, nj, step + 1))
+                    visited.add((ni, nj))
         return -1
     
