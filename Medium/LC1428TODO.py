0165548334917f54848468e6c72a284b3575d3c8
Shuowen Wei <weisw9@gmail.com>
2022-03-27 23:29:43 -0400

    1428

---
+++ Medium/LC1428TODO.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/leftmost-column-with-at-least-a-one/
+
+refer to https://snowan.gitbook.io/study-notes/leetcode/30daychallenge/leftmost-column-with-at-least-a-one
+
+"""
+
