d7e6e4926aa4c7e672262e099d9b998a7e0c44a1
Shuowen Wei <weisw9@gmail.com>
2022-04-05 23:09:10 -0400

    92

--- Medium/LC92.py
+++ Medium/LC92.py
@@ -67,3 +67,24 @@ class Solution(object):
         # pre is the new head, 
         # cur is till
         """
+        
+        # solution 2: recursively
+        self.successor = ListNode(-1)
+        def reverseN(head, n):
+            if n == 1:
+                # // 记录第 n + 1 个节点
+                self.successor = head.next
+                return head
+            # // 以 head.next 为起点，需要反转前 n - 1 个节点
+            last = reverseN(head.next, n - 1)
+            head.next.next = head
+            # // 让反转之后的 head 节点和后面的节点连起来
+            head.next = self.successor
+            return last
+        
+        if left == 1:
+            return reverseN(head, right)
+        # // 前进到反转的起点触发 base case
+        head.next = self.reverseBetween(head.next, left - 1, right - 1)
+        return head
+    
