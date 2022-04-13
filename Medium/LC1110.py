2e6431f00b4375983ea03135204a4253835d3976
Shuowen Wei <weisw9@gmail.com>
2022-04-12 23:09:44 -0400

    1110

---
+++ Medium/LC1110.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/delete-nodes-and-return-forest/
+
+refer to https://leetcode.com/problems/delete-nodes-and-return-forest/discuss/328854/Python-Recursion-with-explanation-Question-seen-in-a-2016-interview
+
+https://leetcode.com/problems/delete-nodes-and-return-forest/discuss/328853/JavaC%2B%2BPython-Recursion-Solution
+If a node is root (has no parent) and isn't deleted, when will we add it to the result.
+"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def delNodes(self, root, to_delete):
+        """
+        :type root: TreeNode
+        :type to_delete: List[int]
+        :rtype: List[TreeNode]
+        """
+        dct_to_delete = {node for node in to_delete}
+        
+        res = []
+        def dfs(node, is_root):
+            if node is None:
+                return None
+            isInDelete = node.val in dct_to_delete
+            if is_root is True and isInDelete is False:
+                res.append(node)
+            if isInDelete:
+                node.left = dfs(node.left, True)
+                node.right = dfs(node.right, True)
+                return None
+            else:
+                node.left = dfs(node.left, False)
+                node.right = dfs(node.right, False)
+                return node
+        dfs(root, True)
+        return res 
+            
+            
+        
