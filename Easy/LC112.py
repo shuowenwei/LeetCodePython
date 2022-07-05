51d8fe6efd9da54d60df1a437f51d0b518b9c553
Shuowen Wei <weisw9@gmail.com>
2022-07-05 18:51:35 -0400

    updated resume

---
+++ Easy/LC112.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wei, Shuowen
+
+https://leetcode.com/problems/path-sum/
+
+"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def hasPathSum(self, root, targetSum):
+        """
+        :type root: TreeNode
+        :type targetSum: int
+        :rtype: bool
+        """
+        def dfs(node, targetSum):
+            if node is None:
+                return False
+            elif targetSum == node.val and node.left is None and node.right is None:
+                return True
+            else:
+                return dfs(node.left, targetSum - root.val) or dfs(node.right, targetSum - root.val)
+        
+        return dfs(root, targetSum)
+
+        # def dfs(node, targetSum, curSum):
+        #     if node is None:
+        #         return False
+        #     elif targetSum == curSum and node.left is None and node.right is None:
+        #         return True
+        #     else:
+        #         print(targetSum, curSum, root.val)
+        #         return dfs(node.left, targetSum, curSum + root.val) or dfs(node.right, targetSum, curSum + root.val)
+        
+        # return dfs(root, targetSum, 0)
