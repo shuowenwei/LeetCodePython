4db7a2f4b8a2092dc89ab5af43b65049d848bdb2
Shuowen Wei <weisw9@gmail.com>
2022-07-28 15:45:56 -0400

    preSum

--- Hard/LC2050.py
+++ Hard/LC2050.py
@@ -7,6 +7,7 @@ https://leetcode.com/problems/parallel-courses-iii/
 https://leetcode.com/problems/parallel-courses-iii/discuss/1537479/C%2B%2BPython-Topology-Sort-O(M%2B-N)-Clean-and-Concise
 
 LC1834, LC2050, LC630
+topology sort
 """
 class Solution(object):
     def minimumTime(self, n, relations, time):
@@ -16,27 +17,25 @@ class Solution(object):
         :type time: List[int]
         :rtype: int
         """
+        import collections
         graph = collections.defaultdict(list)
         inDegree = [0] * n
-        for s, d in relations:
-            graph[s-1].append(d-1)
-            inDegree[d-1] += 1
+        for prevCourse, nextCourse in relations:
+            # course index shift by 1
+            graph[prevCourse-1].append(nextCourse-1)
+            inDegree[nextCourse-1] += 1
             
-            
-        distant = [0] * n
-        q = deque()
-        for node in range(n):
-            if inDegree[node] == 0:
-                distant[node] = time[node]
-                q.append(node)
+        startCourses = [c for c in range(n) if inDegree[c] == 0]
+        minMonths = time[::] #same: [time[c] if inDegree[c] == 0 else 0 for c in range(n)]
+        q = collections.deque(startCourses)
 
-        while q:
-            cur = q.popleft()
-            for nxt in graph[cur]:
-                distant[nxt] = max(distant[nxt], distant[cur] + time[nxt] )
-                inDegree[nxt] -= 1
-                if inDegree[nxt] == 0:
-                    q.append(nxt)
-
-        return max(distant)
+        while q: 
+            curCourse = q.popleft()
+            for nextCourse in graph[curCourse]:
+                minMonths[nextCourse] = max(minMonths[nextCourse], minMonths[curCourse] + time[nextCourse])
+                inDegree[nextCourse] -= 1 
+                if inDegree[nextCourse] == 0:
+                    q.append(nextCourse)
+                    
+        return max(minMonths)
 
