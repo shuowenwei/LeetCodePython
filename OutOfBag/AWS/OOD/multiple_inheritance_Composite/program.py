daa22cf708514ddb72302c35366daaf0bea322ac
Shuowen Wei <weisw9@gmail.com>
2022-07-28 21:19:08 -0400

    address

--- OutOfBag/AWS/OOD/multiple_inheritance_Composite/program.py
+++ OutOfBag/AWS/OOD/multiple_inheritance_Composite/program.py
@@ -1,13 +1,28 @@
+# In program.py
+
 import hr
 import employees
 import productivity
+import contacts
 
 manager = employees.Manager(1, 'Mary Poppins', 3000)
+manager.address = contacts.Address(
+    '121 Admin Rd', 
+    'Concord', 
+    'NH', 
+    '03301'
+)
 secretary = employees.Secretary(2, 'John Smith', 1500)
+secretary.address = contacts.Address(
+    '67 Paperwork Ave.', 
+    'Manchester', 
+    'NH', 
+    '03101'
+)
 sales_guy = employees.SalesPerson(3, 'Kevin Bacon', 1000, 250)
 factory_worker = employees.FactoryWorker(4, 'Jane Doe', 40, 15)
 temporary_secretary = employees.TemporarySecretary(5, 'Robin Williams', 40, 9)
-company_employees = [
+employees = [
     manager,
     secretary,
     sales_guy,
@@ -15,6 +30,6 @@ company_employees = [
     temporary_secretary,
 ]
 productivity_system = productivity.ProductivitySystem()
-productivity_system.track(company_employees, 40)
+productivity_system.track(employees, 40)
 payroll_system = hr.PayrollSystem()
-payroll_system.calculate_payroll(company_employees)
+payroll_system.calculate_payroll(employees)
