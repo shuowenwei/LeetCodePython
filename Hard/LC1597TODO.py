1e47408171eacb33b30a208b30f54b02b8e6c4b5
Shuowen Wei <weisw9@gmail.com>
2022-07-31 01:31:24 -0400

    1597

--- Hard/LC1597TODO.py
+++ Hard/LC1597TODO.py
@@ -4,6 +4,8 @@
 
 https://leetcode.com/problems/build-binary-expression-tree-from-infix-expression/
 
+https://leetcode.ca/all/1597.html
+refer to https://leetcode.ca/2020-04-14-1597-Build-Binary-Expression-Tree-From-Infix-Expression/
 """
 class Node(object):
     def __init__(self, val, left=None, right=None):
@@ -30,11 +32,11 @@ class Solution(object):
         operatorStack = []
         nodeStack = []
         for char in s: 
-            # print(char, 'char')
+            print('char',char)
             if char == '(':
                 operatorStack.append(char)
             elif char.isdigit():
-                newNode = Node(int(char))
+                newNode = Node(char)
                 nodeStack.append(newNode)
             elif char == ')':
                 while operatorStack[-1] != '(':
@@ -42,25 +44,30 @@ class Solution(object):
                 checkIfIsLeft = operatorStack.pop() # pop out '('
                 assert checkIfIsLeft == '(', f'error here: {checkIfIsLeft}'
             else: # char is in '+-*/'
-                while len(operatorStack) > 0 and dctOpertor2Priority[operatorStack[-1]] > dctOpertor2Priority[char]:
+                while len(operatorStack) > 0 and dctOpertor2Priority[operatorStack[-1]] >= dctOpertor2Priority[char]: # // @note: must be >=, for test case "1+2+3+4+5"
                     self.combine(operatorStack, nodeStack)
                 operatorStack.append(char)
-        
+            print('operation stack', operatorStack)
+            print('node values', [n.val for n in nodeStack])
+            
         while len(nodeStack) > 1:
             self.combine(operatorStack, nodeStack)
+            print('final operation stack', operatorStack)
+            print('final node values', [n.val for n in nodeStack])
         return nodeStack[-1]
 
 sol = Solution()
 s = '2-3/(5*2)+1'
-s = "3*4-2*5"
+# s = "3*4-2*5"
 res = sol.exoTree(s)
 print(res.val)
 
 def traverseTree(root):
     if root is None:
-        return 
-    traverseTree(root.left)
+        print('null')
+        return
     print(root.val)
+    traverseTree(root.left)
     traverseTree(root.left)
 
 traverseTree(res)
