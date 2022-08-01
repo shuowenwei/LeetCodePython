8551f3cf4e63df1540c6d8e36047d582cdb5a0f4
Shuowen Wei <weisw9@gmail.com>
2022-08-01 18:49:23 -0400

    amazon locker

---
+++ OutOfBag/AWS/OOD/AmazonLocker.py
@@ -0,0 +1,14 @@
+"""
+https://www.youtube.com/watch?v=b7riKazRJkU&t=2939s
+
+Amazon Locker Problem
+
+To monoitor are process of how to put the package into a right locker. And one locker for one package. Your 
+package and locker have different size, you need to make sure the locker size > package. 
+
+Requirements:
+1. The delivery guy should be able to find an "optimal" locker for a package/packages.
+2. System should send code to the user
+3. Users should be able to use a code to open a locker and pick up a package
+
+"""
