4ea8b8418f3715b09c13fbf01fe6c1d907913118
Shuowen Wei <weisw9@gmail.com>
2022-08-14 22:25:53 -0400

    adapter

---
+++ DesignPatternsJava/Structural/Adapter/1-Adapter_Pattern/AdapterPatternDemo.java
@@ -0,0 +1,12 @@
+//This is the client class.
+
+public class AdapterPatternDemo {	
+	public static void main(String args[]) 
+	{
+		CreditCard targetInterface=new BankCustomer();
+		
+	    targetInterface.giveBankDetails();
+		
+		System.out.print(targetInterface.getCreditCard());	
+	}
+}
---
+++ DesignPatternsJava/Structural/Adapter/1-Adapter_Pattern/CreditCard.java
@@ -0,0 +1,6 @@
+// this is the target interface.
+public interface CreditCard 
+{
+	public void giveBankDetails();
+	public String getCreditCard();	
+}
