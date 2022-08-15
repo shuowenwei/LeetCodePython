53e4a8640e6c7c8b4d1fa69c547b81cb9cb5ae14
Shuowen Wei <weisw9@gmail.com>
2022-08-14 22:49:47 -0400

    composite

---
+++ DesignPatternsJava/Structural/Composite/3-Composite_Pattern/Employee.java
@@ -0,0 +1,20 @@
+
+public interface Employee 
+
+{
+	
+	public  int getId();
+	
+	public String getName();
+	
+	public double getSalary();
+	
+    public void print();
+	
+	public void add(Employee employee);
+	
+    public void remove(Employee employee);
+    
+    public Employee getChild(int i);
+   
+}
