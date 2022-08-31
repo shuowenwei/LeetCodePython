0e1d591e67a041667f4711da82dbf8e289862c15
Shuowen Wei <weisw9@gmail.com>
2022-08-31 01:10:05 -0400

    Memento

---
+++ DesignPatternsJava/Behavioral/Memento/Memento.java
@@ -0,0 +1,14 @@
+//This is a class.  
+  
+public class Memento {  
+      
+    private String state;  
+  
+    public Memento(String state) {  
+        this.state=state;  
+    }  
+    public String getState() {  
+        return state;  
+    }  
+      
+}// End of the Memento class.  
