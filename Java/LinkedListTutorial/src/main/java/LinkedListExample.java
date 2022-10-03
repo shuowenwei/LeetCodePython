7223c9861062aa39be1a7693e8f68b37a97ce00e
Shuowen Wei <weisw9@gmail.com>
2022-10-03 18:27:17 -0400

    array vs linked

--- Java/LinkedListTutorial/src/main/java/LinkedListExample.java
+++ Java/LinkedListTutorial/src/main/java/LinkedListExample.java
@@ -4,9 +4,19 @@ import java.util.LinkedList;
 public class LinkedListExample {
     public static void main(String[] args){
         LinkedList<String> namesLinkedList = new LinkedList<>();
+        namesLinkedList.add("John");
+        namesLinkedList.add("Paul");
+        namesLinkedList.add("George");
+        namesLinkedList.add("Ringo");
+        System.out.println(namesLinkedList.get(2));
 
-        ArrayList<String> namesArrayList = new ArrayList<>();
+        String[] names = new String[4]; // array is has a fixed size
+        ArrayList<String> namesArrayList = new ArrayList<>(); // default size is 10?
+        namesArrayList.add("John");
+        namesArrayList.add("Paul");
+        namesArrayList.add("George");
+        namesArrayList.add("Ringo");
+        System.out.println(namesArrayList.get(2));
 
-        
     }
 }
