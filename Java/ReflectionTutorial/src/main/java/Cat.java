385ded928d074ad2fcf101a42740699a42486ee6
Shuowen Wei <weisw9@gmail.com>
2022-10-04 16:30:08 -0400

    update cat

--- Java/ReflectionTutorial/src/main/java/Cat.java
+++ Java/ReflectionTutorial/src/main/java/Cat.java
@@ -10,4 +10,20 @@ public class Cat {
     public String getName(){
         return name;
     }
+
+    public void meow(){
+        System.out.println("Meow");
+    }
+
+    private void heyThisIsPrivate(){
+        System.out.println("How did you call this??");
+    }
+
+    public static void thisIsAPublicStaticMethod() {
+        System.out.println("I'm public and static!");
+    }
+
+    private static void thisIsAPrivateStaticMethod(){
+        System.out.println("Hey, I'm private and static!");
+    }
 }
