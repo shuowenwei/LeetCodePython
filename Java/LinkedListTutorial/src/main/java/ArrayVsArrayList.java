9d0e26cc76ef9563ce4f5f176634d95ce362cb13
Shuowen Wei <weisw9@gmail.com>
2022-10-03 23:22:46 -0400

    ArrayList

--- Java/LinkedListTutorial/src/main/java/ArrayVsArrayList.java
+++ Java/LinkedListTutorial/src/main/java/ArrayVsArrayList.java
@@ -11,11 +11,33 @@ public class ArrayVsArrayList {
         ArrayList<String> friendsArrayList2 =
                 new ArrayList<>(Arrays.asList("John", "Chris", "Eric", "Luke"));
 
+        // Get element
         System.out.println(friendsArray2[1]);
         System.out.println(friendsArrayList2.get(1));
 
+        // Get size
         System.out.println(friendsArray2.length);
         System.out.println(friendsArrayList2.size());
 
+        // Add an element
+        // you can't do that with Arrays
+        friendsArrayList2.add("Mitch");
+        System.out.println(friendsArrayList2.get(4));
+
+        // Set an element
+        friendsArray2[0] = "Carl";
+        System.out.println(friendsArray2[0]);
+        friendsArrayList2.set(0, "Carl");
+        System.out.println(friendsArrayList2.get(0));
+
+        // Remove an element
+        // you can't do that with Arrays
+        friendsArrayList2.remove(1);
+        friendsArrayList2.remove("Chris");
+        System.out.println(friendsArrayList2.get(1));
+
+        // Print
+        System.out.println(friendsArray); // give the physical address
+        System.out.println(friendsArrayList2);
     }
 }
