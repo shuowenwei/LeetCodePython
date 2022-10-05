6aa90b66d5091e72f8fc77f70ae7c469587aa374
Shuowen Wei <weisw9@gmail.com>
2022-10-04 20:12:01 -0400

    reflection

--- Java/ReflectionTutorial/src/main/java/ReflectionTutorial.java
+++ Java/ReflectionTutorial/src/main/java/ReflectionTutorial.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ReflectionTutorial {
+
+import java.lang.reflect.Field;
+
+public class ReflectionTutorial {
+
+    public static void main(String[] args){
+        Cat myCat = new Cat("Stella", 6);
+
+        Field[] catFields = myCat.getClass().getDeclaredFields();
+
+        for (Field field : catFields) {
+            System.out.println(field.getName());
+        }
+    }
 }
