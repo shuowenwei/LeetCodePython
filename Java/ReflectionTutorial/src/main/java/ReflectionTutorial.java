0821af55eca60084b503b5f751301048c55e4381
Shuowen Wei <weisw9@gmail.com>
2022-10-06 23:53:54 -0400

    ReflectionTutorial todo

--- Java/ReflectionTutorial/src/main/java/ReflectionTutorial.java
+++ Java/ReflectionTutorial/src/main/java/ReflectionTutorial.java
@@ -4,12 +4,15 @@ import java.lang.reflect.Field;
 public class ReflectionTutorial {
 
     public static void main(String[] args){
-        Cat myCat = new Cat("Stella", 6);
+        Cat myCat = new Cat("Stella", 6); 
 
         Field[] catFields = myCat.getClass().getDeclaredFields();
 
         for (Field field : catFields) {
-            System.out.println(field.getName());
+            // System.out.println(field.getName());
+            if (field.getName().equals("name")){
+                field.set(myCat, "Jimmy McGill");
+            }
         }
     }
 }
